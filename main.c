cc7efe452003825a5ab1d7ab24ec4809b818c8f0
Brian S. Dean <bsd@bsdhome.com>
2003-02-21 21:19:56 +0000

    Update comment due to removal of the default parallel port pin config.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk/avrdude@219 81a1dc3b-b13d-400b-aceb-764788c761c2

--- main.c
+++ main.c
@@ -22,47 +22,10 @@
 /*
  * Code to program an Atmel AVR AT90S device using the parallel port.
  *
- * Pin definitions can be changed via a config file.  Below is the
- * default pin configuration in the absence of a config definition
- * which lists "default" as one of its ids.
- *
- *  Parallel Port      Programmer Function
- *  -------------      -----------------------------
- *       Pins 2-5  ->  Vcc (see note below)
- *       Pin    7  ->  AVR /RESET
- *       Pin    8  ->  AVR SCK  (clock input)
- *       Pin    9  ->  AVR MOSI (instruction in)
- *       Pin   10  <-  AVR MISO (data out)
- *       Pin   18      Signal Ground
- *
- * Additionally, the following connections can be made to enable
- * additional features, however, to enable these features use the
- * pin configuration id "alf" ("-c alf" on the command line):
- *
- *  Parallel Port      Programmer Function
- *  -------------      -----------------------------
- *       Pin    1      STATUS LED, active low (program or verify error)
- *       Pin    6  ->  /ENABLE ('367 bus driver)
- *       Pin   14      STATUS LED, active low (ready)
- *       Pin   16      STATUS LED, active low (programming)
- *       Pin   17      STATUS LED, active low (verifying)
- *
- *  Pin 6 can be tied to the enable line of a 74HC367 in order to
- *  isolate and buffer the data to and from the PC parallel port.
- *  This is useful for connecting to a device in-circuit, and keeps
- *  the state of the parallel port pins from interfering with the
- *  normal operation of the target system.  When programming is
- *  complete, this pin is driven high, causing to pins of the '367 to
- *  float.
- *
- *  NOTE on Vcc connection: make sure your parallel port can supply an
- *  adequate amount of current to power your device.  6-10 mA per pin
- *  is common for parallel port signal lines, but is not guaranteed,
- *  especially for notebook computers.  For additional power, use
- *  multiple pins tied together with Schottky diodes.  If in doubt,
- *  don't risk damaging your parallel port, use an external power
- *  supply.
- * 
+ * For parallel port connected programmers, the pin definitions can be
+ * changed via a config file.  See the config file for instructions on
+ * how to add a programmer definition.
+ *  
  */
 
 #include "ac_cfg.h"
