2f1ab78f45c2280f536c1d7430326bb7524d1534
Brian S. Dean <bsd@bsdhome.com>
2004-12-22 01:52:45 +0000

    Update code copyrights.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk@438 81a1dc3b-b13d-400b-aceb-764788c761c2

--- avrdude/pgm.c
+++ avrdude/pgm.c
@@ -1,6 +1,6 @@
 /*
  * avrdude - A Downloader/Uploader for AVR device programmers
- * Copyright (C) 2002, 2003  Brian S. Dean <bsd@bsdhome.com>
+ * Copyright (C) 2002-2004  Brian S. Dean <bsd@bsdhome.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
