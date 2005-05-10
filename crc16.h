3a3118953bcc38ae84cd7fb5ba5b1a20995b9f5d
Joerg Wunsch <j@uriah.heep.sax.de>
2005-05-10 19:43:44 +0000

    I forgot to add these files in the previous commit.  They are basically obtained from Atmel's appnote AVR067.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk/avrdude@452 81a1dc3b-b13d-400b-aceb-764788c761c2

---
+++ crc16.h
@@ -0,0 +1,25 @@
+#ifndef CRC16_H
+#define CRC16_H
+/*
+ * Derived from CRC algorithm for JTAG ICE mkII, published in Atmel
+ * Appnote AVR067.  Converted from C++ to C.
+ */
+
+extern unsigned short crcsum(const unsigned char* message,
+			     unsigned long length,
+			     unsigned short crc);
+/*
+ * Verify that the last two bytes is a (LSB first) valid CRC of the
+ * message.
+ */
+extern int crcverify(const unsigned char* message,
+		     unsigned long length);
+/*
+ * Append a two byte CRC (LSB first) to message.  length is size of
+ * message excluding crc.  Space for the CRC bytes must be allocated
+ * in advance!
+ */
+extern void crcappend(unsigned char* message,
+		      unsigned long length);
+
+#endif
