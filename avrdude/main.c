e7ee18fa4be2c8b64e51ca729c0883889d1d1c17
Brian S. Dean <bsd@bsdhome.com>
2005-05-14 14:49:39 +0000

    Change the default port to 'serial' for the newly added serial programmers stk500v2 and jtagmkii.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk@466 81a1dc3b-b13d-400b-aceb-764788c761c2

--- avrdude/main.c
+++ avrdude/main.c
@@ -1044,8 +1044,10 @@ int main(int argc, char * argv [])
     exit(1);
   }
 
-  if ((strcmp(pgm->type, "STK500") == 0)
-      || (strcmp(pgm->type, "avr910") == 0)){
+  if ((strcmp(pgm->type, "STK500") == 0) ||
+      (strcmp(pgm->type, "avr910") == 0) ||
+      (strcmp(pgm->type, "STK500V2") == 0) ||
+      (strcmp(pgm->type, "JTAGMKII") == 0)) {
     if (port == default_parallel) {
       port = default_serial;
     }
