852c53d4ac24b29d74b36ce1ad7d527b89caae97
Joerg Wunsch <j@uriah.heep.sax.de>
2009-08-17 04:15:08 +0000

    usbdev_close(): Repair the logic around the conditional compilation of usb_reset() introduced in r798.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk/avrdude@841 81a1dc3b-b13d-400b-aceb-764788c761c2

--- usb_libusb.c
+++ usb_libusb.c
@@ -239,7 +239,7 @@ static void usbdev_close(union filedescriptor *fd)
 
   (void)usb_release_interface(udev, usb_interface);
 
-#if defined(__FreeBSD__)
+#if !defined(__FreeBSD__)
   /*
    * Without this reset, the AVRISP mkII seems to stall the second
    * time we try to connect to it.  This is not necessary on
