10bf04f7064a2bf8e28a71071fc3e895f2db847d
Joerg Wunsch <j@uriah.heep.sax.de>
2010-01-08 16:31:54 +0000

    bug #27505: serbb_posix does not cope with inverted pins * serbb_posix (serbb_highpulsepin): apply PIN_MASK when checking pin numbers. * serbb_win32 (serbb_highpulsepin): (Dito.)
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk@896 81a1dc3b-b13d-400b-aceb-764788c761c2

--- avrdude/serbb_win32.c
+++ avrdude/serbb_win32.c
@@ -223,8 +223,8 @@ static int serbb_getpin(PROGRAMMER * pgm, int pin)
 
 static int serbb_highpulsepin(PROGRAMMER * pgm, int pin)
 {
-        if (pin < 1 || pin > 7)
-                return -1;
+        if ( (pin & PIN_MASK) < 1 || (pin & PIN_MASK) > DB9PINS )
+          return -1;
 
         serbb_setpin(pgm, pin, 1);
 	if (pgm->ispdelay > 1)
