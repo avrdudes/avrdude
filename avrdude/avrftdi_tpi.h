272e78ac002a99b41f0a332c2f0de75bf67ce056
Hannes Weisbach <hannes_weisbach@gmx.net>
2013-05-06 12:50:42 +0000

    avrftdi_tpi.[c|h]: integrate avr_tpi_[program_enable,chip_erase]()
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk@1168 81a1dc3b-b13d-400b-aceb-764788c761c2

--- avrdude/avrftdi_tpi.h
+++ avrdude/avrftdi_tpi.h
@@ -5,8 +5,6 @@
 
 //int avrftdi_tpi_write_byte(PROGRAMMER * pgm, unsigned char byte);
 //int avrftdi_tpi_read_byte(PROGRAMMER * pgm, unsigned char * byte);
-int avrftdi_tpi_program_enable(PROGRAMMER * pgm, AVRPART * p);
-int avrftdi_tpi_chip_erase(PROGRAMMER * pgm, AVRPART * p);
 int avrftdi_cmd_tpi(PROGRAMMER * pgm, unsigned char cmd[], int cmd_len,
 		unsigned char res[], int res_len);
 int avrftdi_tpi_initialize(PROGRAMMER * pgm, AVRPART * p);
