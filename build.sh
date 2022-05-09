a23055d6481f268f1782a55303cc5427072bbe1e
Charles <hallard04@free.fr>
2022-05-09 14:51:16 +0200

    fix typo

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ case "${ostype}" in
             # Apple M1 (may be new version of homebrew also)
             if [ -d /opt/homebrew ]  
             then
-                build_flags="${build_flags} -D CMAKE_C_FLAGS=-I/usr/homebrew/include -D CMAKE_EXE_LINKER_FLAGS=-L/usr/homebrew/Cellar"
+                build_flags="${build_flags} -D CMAKE_C_FLAGS=-I/opt/homebrew/include -D CMAKE_EXE_LINKER_FLAGS=-L/opt/homebrew/Cellar"
             else
                 build_flags="${build_flags} -D CMAKE_C_FLAGS=-I/usr/local/include -D CMAKE_EXE_LINKER_FLAGS=-L/usr/local/Cellar"
             fi
