2d6ec9e5b1e7584914f2558eb5237d7f0fcadde1
Stefan Rueger <stefan.rueger@urclocks.com>
2022-10-21 12:10:41 +0100

    Fix typo in stk500.c comment

--- src/stk500.c
+++ src/stk500.c
@@ -695,7 +695,7 @@ static int stk500_loadaddr(const PROGRAMMER *pgm, const AVRMEM *mem, unsigned in
        * Paper over a bug in some bootloaders: ensure next paged r/w will load ext addr again if
        * page sits just below 64k a boundary. Some bootloaders mistakenly increment their copy of
        * ext_addr_byte in that situation, eg, when they use elpm rx,Z+ to read a byte from flash
-       * and they keep ext_addr_byte in Z. So, if an upload with automated verify finishes just
+       * and they keep ext_addr_byte in RAMPZ. So, if an upload with automated verify finishes just
        * below 64k, AVRDUDE still holds ext_addr_byte at the current 64k segment whilst it's copy
        * in the bootloader has been auto-incremented. Verifying the code from start exposes the
        * discrepancy. Below intervention forces the next r/w to send the correct ext_addr_byte.
