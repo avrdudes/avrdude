c451d91a829ab647667da6e6705157c064d41a82
Stefan Rueger <stefan.rueger@urclocks.com>
2023-04-05 10:19:57 +0100

    Resolve conflicts

--- src/pgm_type.c
+++ src/pgm_type.c
@@ -115,9 +115,12 @@ const PROGRAMMER_TYPE programmers_types[] = { // Name(s) the programmers call th
 const PROGRAMMER_TYPE * locate_programmer_type(const char * id)
 {
   const PROGRAMMER_TYPE * p = NULL;
-  int found = 0;
+  int i;
+  int found;
 
-  for(size_t i = 0; i < sizeof(programmers_types)/sizeof(programmers_types[0]) && !found; i++) {
+  found = 0;
+
+  for (i = 0; i < sizeof(programmers_types)/sizeof(programmers_types[0]) && !found; i++) {
     p = &(programmers_types[i]);
     if (strcasecmp(id, p->id) == 0)
         found = 1;
@@ -131,7 +134,7 @@ const PROGRAMMER_TYPE * locate_programmer_type(const char * id)
 
 // Return type id given the init function or "" if not found
 const char *locate_programmer_type_id(void (*initpgm)(PROGRAMMER *pgm)) {
-  for (size_t i=0; i < sizeof programmers_types/sizeof*programmers_types; i++)
+  for (int i=0; i < sizeof programmers_types/sizeof*programmers_types; i++)
     if(programmers_types[i].initpgm == initpgm)
       return programmers_types[i].id;
 
@@ -165,8 +168,9 @@ void walk_programmer_types(LISTID programmer_types, walk_programmer_types_cb cb,
 void walk_programmer_types(walk_programmer_types_cb cb, void *cookie)
 {
   const PROGRAMMER_TYPE * p;
+  int i;
 
-  for (size_t i = 0; i < sizeof(programmers_types)/sizeof(programmers_types[0]); i++) {
+  for (i = 0; i < sizeof(programmers_types)/sizeof(programmers_types[0]); i++) {
     p = &(programmers_types[i]);
     cb(p->id, p->desc, cookie);
   }
