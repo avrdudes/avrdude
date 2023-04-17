5c85a5a2fb0eea9291f184be6a1e6fe06a136a36
MCUdude <hansibull@gmail.com>
2023-04-17 19:36:19 +0200

    Add HAS_AREF_ADJ

--- src/libavrdude.h
+++ src/libavrdude.h
@@ -216,6 +216,7 @@ typedef struct opcode {
 #define HAS_VTARG_ADJ          4
 #define HAS_VTARG_READ         8
 #define HAS_FOSC_ADJ          16
+#define HAS_AREF_ADJ          32
 
 #define AVR_FAMILYIDLEN 7
 #define AVR_SIBLEN 16
