6215aa3c8b5893349c2dedbc3a70cbe6d7efbb3c
Stefan Rueger <stefan.rueger@urclocks.com>
2023-06-10 13:12:53 +0100

    Add credentials	for partial rewrite

--- src/fileio.c
+++ src/fileio.c
@@ -1,6 +1,7 @@
 /*
  * avrdude - A Downloader/Uploader for AVR device programmers
  * Copyright (C) 2000-2004  Brian S. Dean <bsd@bsdhome.com>
+ * Copyright (C) 2023 Stefan Rueger <stefan.rueger@urclocks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
