82f949dcaadf0b86e0e1dc54aee9f1d01ab40d51
Stefan Rueger <stefan.rueger@urclocks.com>
2023-06-28 17:22:59 +0100

    Remove comma in copyright note

--- src/dryrun_private.h
+++ src/dryrun_private.h
@@ -1,6 +1,6 @@
 /*
  * avrdude - A Downloader/Uploader for AVR device programmers
- * Copyright (C) 2023, Stefan Rueger <stefan.rueger@urclocks.com>
+ * Copyright (C) 2023 Stefan Rueger <stefan.rueger@urclocks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
