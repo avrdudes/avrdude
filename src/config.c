65024b5efa69a4cd6ddd54c0039dfa747d0080cb
Stefan Rueger <stefan.rueger@urclocks.com>
2023-06-28 17:23:22 +0100

    Add Stefan Rueger to copyright info

--- src/config.c
+++ src/config.c
@@ -1,6 +1,7 @@
 /*
  * avrdude - A Downloader/Uploader for AVR device programmers
  * Copyright (C) 2000-2004  Brian S. Dean <bsd@bdmicro.com>
+ * Copyright (C) 2022 Stefan Rueger <stefan.rueger@urclocks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
