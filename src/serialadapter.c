d234bd1e2b6b62dcfec50c270da65ce6b218be30
MCUdude <hansibull@gmail.com>
2023-09-04 15:05:05 +0200

    Utilize usbsn if specified in avrduderc

--- src/serialadapter.c
+++ src/serialadapter.c
@@ -94,15 +94,19 @@ int setport_from_serialadapter(char **portp, const SERIALADAPTER *ser, const cha
         if (sp[i].pid == *(int *)(ldata(usbpid))) {
           sp[i].match = true;
           // SN present
-          if (sernum && sernum[0]) {
+          if ((sernum && sernum[0]) || ser->usbsn[0]) {
             // SN matches
             if ((sp[i].sernum && str_starts(sp[i].sernum, sernum)) ||
-               (str_starts(sernum , "...") && str_ends(sp[i].sernum, sernum+3)))
+               (str_starts(sernum , "...") && str_ends(sp[i].sernum, sernum+3)) ||
+               (sp[i].sernum &&str_eq(sernum, ser->usbsn) && str_eq(sp[i].sernum, sernum)))
               sp[i].match = true;
             // SN does not match
             else
               sp[i].match = false;
           }
+          // SN present in avrduderc but did not match
+          else if (!sernum[0] && ser->usbsn[0])
+            sp[i].match = false;
         }
       }
     }
