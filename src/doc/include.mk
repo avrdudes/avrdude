596c5d039cd23f8439d93d977cddaca65484a493
Hans Ulrich Niedermann <hun@n-dimensional.de>
2024-02-20 06:07:02 +0100

    test whether cross compiling before running compiled avrdude

--- src/doc/include.mk
+++ src/doc/include.mk
@@ -37,34 +37,39 @@ AM_MAKEINFOHTMLFLAGS += --split=node
 EXTRA_DIST           += %reldir%/avrdude.css
 AM_MAKEINFOHTMLFLAGS += --css-include=$(srcdir)/%reldir%/avrdude.css
 
-EXTRA_DIST += %reldir%/parts_comments.txt
-
 # FIXME: Do not run avrdude_exe if cross-compiling.
 
-CLEANFILES           += %reldir%/programmers.texi
 %C%_avrdude_TEXINFOS += %reldir%/programmers.texi
+if !CROSS_COMPILING
+CLEANFILES           += %reldir%/programmers.texi
 %reldir%/programmers.texi: $(avrdude_exe) $(avrdude_conf) Makefile
 	@$(MKDIR_P) %reldir%
 	$(avrdude_exe) -C $(avrdude_conf) -c '?' 2>&1 \
 	| $(AWK) '$$2 ~ /^=$$/ {printf("@item @code{%s} @tab %s\n",$$1,gensub("[^=]+=[ \t]*","",1))}' \
 	| sed "s# *,\? *<\?\(http://[^ \t>]*\)>\?#,@*\n@url{\1}#g" \
 	>%reldir%/programmers.texi
+endif
 
-CLEANFILES           += %reldir%/programmer_types.texi
 %C%_avrdude_TEXINFOS += %reldir%/programmer_types.texi
+if !CROSS_COMPILING
+CLEANFILES           += %reldir%/programmer_types.texi
 %reldir%/programmer_types.texi: $(avrdude_exe) $(avrdude_conf) Makefile
 	@$(MKDIR_P) %reldir%
 	$(avrdude_exe) -C $(avrdude_conf) -c '?type' 2>&1 \
 	| $(AWK) '$$2 ~ /^=$$/ {printf("@item @code{%s} @tab %s\n",$$1,gensub("[^=]+=[ \t]*","",1))}' \
 	| sed "s#<\?\(http://[^ \t,>]*\)>\?#@url{\1}#g" \
 	>%reldir%/programmer_types.texi
+endif
 
 # FIXME: srcdir or builddir for parts_comments.txt?
-CLEANFILES           += %reldir%/parts.texi
+EXTRA_DIST           += %reldir%/parts_comments.txt
 %C%_avrdude_TEXINFOS += %reldir%/parts.texi
+if !CROSS_COMPILING
+CLEANFILES           += %reldir%/parts.texi
 %reldir%/parts.texi: $(avrdude_exe) $(avrdude_conf) $(srcdir)/%reldir%/parts_comments.txt Makefile
 	@$(MKDIR_P) %reldir%
 	$(avrdude_exe) -C $(avrdude_conf) -p '?' 2>&1 \
 	| $(AWK) '$$2 ~ /^=$$/ {printf("@item @code{%s} @tab %s\n",$$1,$$3)}' \
 	| sed -e "`sed 's:\([^ \t]*\)[ \t]*\(.*\):s/\1$$/\1 \2/g:g' < $(srcdir)/%reldir%/parts_comments.txt`" \
 	>%reldir%/parts.texi
+endif
