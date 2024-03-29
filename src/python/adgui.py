896785a192ee05d6fbcb58a6c5a3cedf63999d30
Joerg Wunsch <j@uriah.heep.sax.de>
2024-03-29 16:31:38 +0100

    Add copyright, add some more "About" text

--- src/python/adgui.py
+++ src/python/adgui.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+# This is a simple GUI demonstrator for the SWIG_Avrdude Python
+# bindings to libavrdude.
+
+# Its main purpose is to demonstrate that these Python bindings
+# provide all the functionality that is needed for a full-featured AVR
+# programming tool with similar features as the CLI version. It is not
+# meant to be complete though, or to be a full replacement for the CLI
+# tool.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 import pathlib
