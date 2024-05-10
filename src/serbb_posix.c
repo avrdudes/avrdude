b4ed989c99ea7fcc44e0f9fa7db82e1150ac46e4
Stefan Rueger <stefan.rueger@urclocks.com>
2024-05-10 14:10:37 +0100

    Make static struct const in serbb_posix.c

--- src/serbb_posix.c
+++ src/serbb_posix.c
@@ -62,11 +62,11 @@ static struct termios oldmode;
 
 #define DB9PINS 9
 
-static int serregbits[DB9PINS + 1] =
+static const int serregbits[DB9PINS + 1] =
 { 0, TIOCM_CD, 0, 0, TIOCM_DTR, 0, TIOCM_DSR, TIOCM_RTS, TIOCM_CTS, TIOCM_RI };
 
 #ifdef DEBUG
-static char *serpins[DB9PINS + 1] =
+static const char *serpins[DB9PINS + 1] =
   { "NONE", "CD", "RXD", "TXD", "DTR", "GND", "DSR", "RTS", "CTS", "RI" };
 #endif
 
