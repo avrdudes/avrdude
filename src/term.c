fa618a8440f57fa06c4b04ea147df39d2ec1434f
Stefan Rueger <stefan.rueger@urclocks.com>
2024-07-07 12:15:19 +0100

    Improve documentation for terminal erase

--- src/term.c
+++ src/term.c
@@ -964,8 +964,9 @@ static int cmd_erase(const PROGRAMMER *pgm, const AVRPART *p, int argc, const ch
     return cmd_write(pgm, p, 6, args);
   }
 
-  term_out("%s chip erase ...\n", (pgm->prog_modes & PM_SPM)?
-    "asking bootloader to perform": "performing");
+  term_out("%s chip erase; discarded pending writes to flash%s\n",
+    (pgm->prog_modes & PM_SPM)? "asking bootloader to perform": "performing",
+    avr_locate_bootrow(p)? ", EEPROM and bootrow": avr_locate_eeprom(p)? "and EEPROM": "");
 
   // Erase chip and clear cache
   int rc = pgm->chip_erase_cached(pgm, p);
