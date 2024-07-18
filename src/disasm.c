966f20b8583be6b72f3463403460933bf4082949
Stefan Rueger <stefan.rueger@urclocks.com>
2024-07-18 23:48:55 +0100

    Adapt AVRDUDE style for GPL 2.0 licence header

--- src/disasm.c
+++ src/disasm.c
@@ -1,29 +1,28 @@
-
 /*
-    avrdisas - A disassembler for AVR microcontroller units
-    Copyright (C) 2007 Johannes Bauer
+ * AVRDUDE - A Downloader/Uploader for AVR device programmers
+ *
+ * This file was ported from
+ * avrdisas - A disassembler for AVR microcontroller units
+ * Copyright (C) 2007 Johannes Bauer <JohannesBauer@gmx.de>
+ *
+ * Copyright (C) 2024 port by Stefan Rueger <stefan.rueger@urclocks.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-    This file is part of avrdisas.
+/* $Id$ */
 
-    avrdisas is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    avrdisas is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with avrdisas; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    Johannes Bauer
-    Mussinanstr. 140
-    92318 Neumarkt/Opf.
-    JohannesBauer@gmx.de
-*/
 
 #include <stdio.h>
 #include <sys/types.h>
