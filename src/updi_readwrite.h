09ddba79abff57c8dc8eccbddb14c4a4b4b0855e
Stefan Rueger <stefan.rueger@urclocks.com>
2024-08-18 01:54:33 +0100

    Reformat spacing/comments in src/updi_readwrite.h

--- src/updi_readwrite.h
+++ src/updi_readwrite.h
@@ -31,19 +31,19 @@
 extern "C" {
 #endif
 
-int updi_read_cs(const PROGRAMMER *pgm, uint8_t address, uint8_t *value);
-int updi_write_cs(const PROGRAMMER *pgm, uint8_t address, uint8_t value);
-int updi_write_key(const PROGRAMMER *pgm, unsigned char *buffer, uint8_t size_type, uint16_t size);
-int updi_read_sib(const PROGRAMMER *pgm, unsigned char *buffer, uint16_t size);
-int updi_read_byte(const PROGRAMMER *pgm, uint32_t address, uint8_t *value);
-int updi_write_byte(const PROGRAMMER *pgm, uint32_t address, uint8_t value);
-int updi_read_data(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
-int updi_write_data(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
-int updi_read_data_words(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
-int updi_write_data_words(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
+  int updi_read_cs(const PROGRAMMER *pgm, uint8_t address, uint8_t *value);
+  int updi_write_cs(const PROGRAMMER *pgm, uint8_t address, uint8_t value);
+  int updi_write_key(const PROGRAMMER *pgm, unsigned char *buffer, uint8_t size_type, uint16_t size);
+  int updi_read_sib(const PROGRAMMER *pgm, unsigned char *buffer, uint16_t size);
+  int updi_read_byte(const PROGRAMMER *pgm, uint32_t address, uint8_t *value);
+  int updi_write_byte(const PROGRAMMER *pgm, uint32_t address, uint8_t value);
+  int updi_read_data(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
+  int updi_write_data(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
+  int updi_read_data_words(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
+  int updi_write_data_words(const PROGRAMMER *pgm, uint32_t address, uint8_t *buffer, uint16_t size);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* updi_readwrite_h */
+#endif
