b09225521810250248fc8d0e266a5a458a42b02a
Hans Ulrich Niedermann <hun@n-dimensional.de>
2024-11-23 04:42:23 +0100

    XXX cmake function with arguments
    Next: Create a output messages file, and a C include file.
    
    TODO: Name the generated files buildinfo-include.c and buildinfo-messages.txt

--- src/build-helpers/buildinfo.cmake
+++ src/build-helpers/buildinfo.cmake
@@ -3,6 +3,13 @@ function(buildinfo_setup)
   message(STATUS "END buildinfo_setup")
 endfunction()
 
+function(buildinfo_item KEY VAL)
+  cmake_parse_arguments(arg "" "" "" ${ARGN})
+  message(STATUS "BEGIN buildinfo_item")
+  message(STATUS "  K,V: ${KEY}, ${VAL}")
+  message(STATUS "END buildinfo_item")
+endfunction()
+
 function(buildinfo_output)
   message(STATUS "BEGIN buildinfo_output")
   message(STATUS "END buildinfo_output")
