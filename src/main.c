baddba9139ed981d753d94659c5e99f849c65f3c
MCUdude <hansibull@gmail.com>
2025-05-01 23:15:27 +0200

    Remove pgm->close(pgm)

--- src/main.c
+++ src/main.c
@@ -1639,9 +1639,8 @@ int main(int argc, char *argv[]) {
       imsg_error(" - use -b to set lower baud rate, e.g. -b %d\n", baudrate? baudrate/2: 57600);
     else if(str_eq(pgm->type, "buspirate_bb") || str_eq(pgm->type, "linuxgpio") ||
       str_eq(pgm->type, "par") || str_eq(pgm->type, "SERBB")) {
-      imsg_error(" - use -i %s to set a longer delay (in microseconds) between each bit state change, e.g. -i 50\n",
-        bitclock? "instead of -B": "");
-      pgm->close(pgm);
+      imsg_error(" - use -i %sto set a longer delay (in microseconds) between each bit state change, e.g. -i 50\n",
+        bitclock? "instead of -B ": "");
     }
     else
       imsg_error(" - use -B to set lower the bit clock frequency, e.g. -B 125kHz\n");
