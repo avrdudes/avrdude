cddc610f7cea5e64a4b6f9feef6934836e139c46
mcuee <xiaofanc@gmail.com>
2026-08-18 10:56:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -122,10 +122,12 @@ cat <<EOF
 
 Build succeeded.
 
-Run
+For Linux, macOS MacPorts and BSDs, you may want to run
 
 sudo cmake --build build_${ostype} --target install
 
 to install.
+
+For Windows and macOS Homebrew, you may want to copy avrdude binary and avrdude.conf file to the right location.
 
 EOF
