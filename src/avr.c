49d04b598a27eddc671812aad01da495872020c3
stefanrueger <stefan.rueger@urclocks.com>
2026-08-25 19:00:13 +0200

    Change detailed verification output to msg_info()

--- src/avr.c
+++ src/avr.c
@@ -1386,19 +1386,17 @@ int avr_verify_mem(const PROGRAMMER *pgm, const AVRPART *p, const AVRPART *v, co
       uint8_t bitmask = is_isp(p)? get_fuse_bitmask(a): avr_mem_bitmask(p, a, i);
 
       if(ro || (pgm->readonly && pgm->readonly(pgm, p, a, i))) {
-        if(quell_progress < 2) {
-          if(vroerror < 10)
-            imsg_warning("  device 0x%02x != input 0x%02x at addr 0x%04x (read-only location: ignored)\n", buf1[i], buf2[i], i);
-          else if(vroerror == 10)
-            imsg_warning("  suppressing further mismatches in read-only areas\n");
-        }
+        if(vroerror < 10)
+          imsg_info("  device 0x%02x != input 0x%02x at addr 0x%04x (read-only location: ignored)\n", buf1[i], buf2[i], i);
+        else if(vroerror == 10)
+          imsg_info("  suppressing further mismatches in read-only areas\n");
         vroerror++;
       } else if((buf1[i] & bitmask) != (buf2[i] & bitmask)) {
         // Mismatch is not just in unused bits
         if(verror < maxerrs)
-          imsg_error("  device 0x%02x != input 0x%02x at addr 0x%04x (error)\n", buf1[i], buf2[i], i);
+          imsg_info("  device 0x%02x != input 0x%02x at addr 0x%04x (error)\n", buf1[i], buf2[i], i);
         else if(verror == maxerrs)
-          imsg_warning("  suppressing further verification errors\n");
+          imsg_info("  suppressing further verification errors\n");
         verror++;
       } else {
         // Mismatch is only in unused bits
