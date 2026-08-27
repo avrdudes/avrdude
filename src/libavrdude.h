99b965f0bd31173b179b62a960a86f6b68156770
stefanrueger <stefan.rueger@urclocks.com>
2026-08-27 23:11:41 +0200

    Provide dryrun_part()

--- src/libavrdude.h
+++ src/libavrdude.h
@@ -1855,6 +1855,8 @@ extern "C" {
   Urboot_template **urboottemplate(const Avrintel *up, const char *mcu, const char *io, const char *blt,
     int req_feat, int req_ulevel, int showall, int *np, int silent);
 
+  AVRPART *dryrun_part(const char *id, int *bootsizep, int init, int random, int hole, int seed);
+
 #ifdef __cplusplus
 }
 #endif
